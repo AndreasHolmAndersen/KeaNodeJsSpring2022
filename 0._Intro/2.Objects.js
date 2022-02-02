3cb2c696e7260832f6324cc006a15af1ab14db27
AndreasHolmAndersen <andreas.holm.andersen@gmail.com>
2022-02-02 10:16:19 +0100

    created homework for 7/2

---
+++ 0._Intro/2.Objects.js
@@ -0,0 +1,44 @@
+// --------------------------------------
+// Objects
+// --------------------------------------
+// Exercise 1 - Retrieve value from object by key
+
+const alienMessage = {
+	message: "Hello, earthling! I bring peace."
+};
+
+// Log the message 
+console.log(alienMessage.message);
+
+// --------------------------------------
+// Exercise 2 - Defining an object. 
+
+// Create an object that has your name and age. 
+const Person = {
+	name: "Andreas",
+	age: 23
+};
+
+
+// --------------------------------------
+// Exercise 3 - Add a property 
+
+const stackOverflow = {
+	isAllowed: true
+};
+
+// make a rule called isAllowed and let the value be true
+
+// --------------------------------------
+// Exercise 4 - Remove a property 
+
+const thisSong = {
+	about: "Just a tribute"
+};
+
+// remove the property "description" and add a property called "about" that should say "Just a tribute." 
+
+
+// --------------------------------------
+
+
--- 0._Intro/test.js
+++
@@ -1,8 +0,0 @@
-const firstName = "Andreas";
-const age = 5;
-
-console.log("Age: ", age);
-
-// type coercion
-
-
