ec5cb7de4287ddc404a3a248ccee5912787f377f
AndreasHolmAndersen <andreas.holm.andersen@gmail.com>
2022-03-15 11:40:21 +0100

    lesson 15/03/22

---
+++ 9._Clientside_Routing/src/App.svelte
@@ -0,0 +1,23 @@
+<script>
+	import { Router, Link, Route } from "svelte-navigator";
+	import Home from "./pages/Home/Home.svelte";
+	import About from "./pages/About/About.svelte";
+
+</script>
+
+<Router>
+	<nav>
+		<Link to="/">Home</Link>
+		<Link to="/about">About</Link>
+	</nav>
+
+	<div>
+		<Route path="/" component={Home}>
+			<Home/>
+		</Route>
+
+		<Route path="/about" component={About}>
+			<About/>
+		</Route>
+	</div>
+</Router>
